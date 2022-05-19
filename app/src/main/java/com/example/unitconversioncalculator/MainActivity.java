f0678f4835fe94180efecc5abbe2267212fe3762
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-05-19 17:25:16 +0500

    spinner addedd

--- app/src/main/java/com/example/unitconversioncalculator/MainActivity.java
+++ app/src/main/java/com/example/unitconversioncalculator/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.unitconversioncalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner unitCategories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        unitCategories=findViewById(R.id.categories);
+
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.Catogories, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
     }
 }
